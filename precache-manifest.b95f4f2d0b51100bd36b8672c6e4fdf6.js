7b3e9b09952dcd88b0a49ccd9d892e0bddbfe73e
Hovav Oppenheim <hovav@bnop.co>
2019-03-16 22:34:21 +0200

    Updates

---
+++ precache-manifest.b95f4f2d0b51100bd36b8672c6e4fdf6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5ced8523ef7ee2ae583",
+    "url": "/Yali9/static/js/runtime~main.a5ced852.js"
+  },
+  {
+    "revision": "93f70432fa5945b44adb",
+    "url": "/Yali9/static/js/main.93f70432.chunk.js"
+  },
+  {
+    "revision": "859b1399f75748facd3b",
+    "url": "/Yali9/static/js/1.859b1399.chunk.js"
+  },
+  {
+    "revision": "93f70432fa5945b44adb",
+    "url": "/Yali9/static/css/main.e5b050e0.chunk.css"
+  },
+  {
+    "revision": "99846d2cf7c94663ff6b487f030fce47",
+    "url": "/Yali9/index.html"
+  }
+];
